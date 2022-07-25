3be91b89d7165e31538a2e08e58b604ab0129ed3
Jakub Beránek <berykubik@gmail.com>
2022-07-25 14:01:16 +0200

    Fix docstring enrichment for methods

--- nedoc/render.py
+++ nedoc/render.py
@@ -58,7 +58,7 @@ class RenderContext:
         lexer = lexers.get_lexer_by_name("python")
         return highlight(code, lexer, formatter)
 
-    def render_markup(self, text):
+    def render_markup(self, text: str):
         if self.gctx.config.markup == Markup.RST:
             content = convert_rst_to_html(text)
             return "<div class='rst-doc'>{}</div>".format(content)
@@ -73,18 +73,18 @@ class RenderContext:
 
     def get_parsed_docstring(self, unit: Unit) -> ParsedDocString:
         docstring = parse_docstring(self.gctx.config.style, unit.docstring)
-        return self.enrich_docstring(docstring)
+        return self.enrich_docstring(unit, docstring)
 
-    def enrich_docstring(self, docstring: ParsedDocString) -> ParsedDocString:
+    def enrich_docstring(self, unit: Unit, docstring: ParsedDocString) -> ParsedDocString:
         """
         Tries to add additional information to the docstring.
 
         Adds types to parameters from type hints.
         Renders markup in parameters description.
         """
-        if isinstance(self.unit, Function):
+        if isinstance(unit, Function):
             if docstring.params is not None:
-                args = {arg.name: arg for arg in self.unit.named_args}
+                args = {arg.name: arg for arg in unit.named_args}
                 for param in docstring.params:
                     if param.description is not None:
                         param.description = self.render_markup(param.description)
